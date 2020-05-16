669748b95ed8df5a9c9009ae1e1b0e482248ad8c
Denis Efremov <efremov@linux.com>
2020-05-16 09:40:02 +0300

    animate: add ProB prefs and more options
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- src/main/java/animate/Animate.java
+++ src/main/java/animate/Animate.java
@@ -29,14 +29,29 @@ public class Animate {
 		this.api = api;
 	}
 
-	public void start(String model_path, int steps) throws Exception {
+	public void start(final String model_path, final int steps, final int size, final boolean perf) throws Exception {
 		System.out.println("ProB version: " + api.getVersion());
 		System.out.println();
 		System.out.println("Load Event-B Machine");
 
 		StateSpace stateSpace = null;
+		Map<String, String> prefs = new HashMap<String, String>() {{
+			put("MEMOIZE_FUNCTIONS", "true");
+			put("SYMBOLIC", "true");
+			put("TRACE_INFO", "true");
+			put("TRY_FIND_ABORT", "true");
+			put("SYMMETRY_MODE", "hash");
+			put("DEFAULT_SETSIZE", String.valueOf(size));
+			put("COMPRESSION", "true");
+			put("CLPFD", "true");
+			put("PROOF_INFO", "true");
+			put("OPERATION_REUSE", "true");
+			if (perf) {
+				put("PERFORMANCE_INFO", "true");
+			}
+		}};
 		try {
-			stateSpace = api.eventb_load(model_path);
+			stateSpace = api.eventb_load(model_path, prefs);
 		} catch (Exception e) {
 			System.out.println("Error loading model: " + e.getMessage());
 			System.exit(3);
@@ -66,10 +81,13 @@ public class Animate {
 	public static void main(String[] args) throws Exception {
 		Options options = new Options();
 		int steps = 5;
+		int size = 4;
 
 		options.addRequiredOption("m", "model", true, "path to model.bum file");
 		options.addOption("d", "debug", false, "enable debug log (default: off)");
 		options.addOption("s", "steps", true, "number of random steps (default: 5)");
+		options.addOption("z", "size", true, "default size for ProB sets (default: 4)");
+		options.addOption("p", "perf", false, "print ProB performance info (default: off)");
 
 		CommandLineParser parser = new DefaultParser();
 		HelpFormatter formatter = new HelpFormatter();
@@ -89,19 +107,23 @@ public class Animate {
 			root.setLevel(Level.WARN);
 		}
 
-		if (cmd.hasOption("steps")) {
-			try {
-				steps = Integer.parseInt(cmd.getOptionValue("steps"));
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				formatter.printHelp("animate", options);
 
-				System.exit(1);
+		try {
+			if (cmd.hasOption("steps")) {
+				steps = Integer.parseInt(cmd.getOptionValue("steps"));
 			}
+			if (cmd.hasOption("size")) {
+				size = Integer.parseInt(cmd.getOptionValue("size"));
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			formatter.printHelp("animate", options);
+
+			System.exit(1);
 		}
 
 		Animate m = INJECTOR.getInstance(Animate.class);
-		m.start(cmd.getOptionValue("model"), steps);
+		m.start(cmd.getOptionValue("model"), steps, size, cmd.hasOption("perf"));
 
 		System.exit(0);
 	}
